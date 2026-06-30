63a4962110d3dc6044a213a218b0c017a6dcf0ce
Manoj Yadav <160408265+ManojYadav2005@users.noreply.github.com>
2026-06-30 11:25:45 +0530

    Time: 3 ms (85.98%), Space: 42 MB (92.69%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    
+    vector<int>rank;
+    vector<int>parent;
+
+int find(int i,vector<int>&parent){
+    if(i==parent[i]) return i;
+
+    return parent[i]=find(parent[i],parent);
+    } 
+
+    void Union(int x,int y,vector<int>&parent,vector<int>&rank){
+    int x_parent=find(x,parent);
+    int y_parent=find(y,parent);
+
+    if(x_parent==y_parent){
+    return ;
+    }
+    if(rank[x_parent]>rank[y_parent]){
+    parent[y_parent]=x_parent;
+    }
+    else if(rank[x_parent]<rank[y_parent]){
+    parent[x_parent]=y_parent;
+    }
+    else{
+    parent[x_parent]=y_parent;
+    rank[y_parent]++;
+    }
+
+    } 
+
+
+
+    int makeConnected(int n, vector<vector<int>>& connections) {
+    
+    if(connections.size()<n-1) return -1;
+    
+    parent.resize(n);
+    rank.resize(n,0);
+
+    for(int i=0;i<n;i++){
+    parent[i]=i;
+    }
+
+    int components=n;
+
+    for(auto &vec:connections){
+    if(find(vec[0],parent)!=find(vec[1],parent)){
+    Union(vec[0],vec[1],parent,rank);
+    components--;
+    }
+    }
+    return components-1;
+    }
+};
