e4c4acb854245b4e7221e81da4fe019bc4f2efef
Manoj Yadav <160408265+ManojYadav2005@users.noreply.github.com>
2026-07-19 10:40:36 +0530

    Time: 0 ms (100%), Space: 8.7 MB (39.5%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
+      if(dividend==INT_MIN && divisor==-1){
+      return INT_MAX;
+      }  
+      bool negative=(dividend<0)^(divisor<0);
 
-        // Overflow case
-        if (dividend==INT_MIN && divisor==-1)
-        return INT_MAX;
-
-        bool negative=(dividend<0)^(divisor<0);
-
-        long long a=llabs((long long)dividend);
-        long long b=llabs((long long)divisor);
-
-        long long ans = 0;
-
-        while (a>=b) {
+      long long a=llabs((long long)dividend);
+      long long b=llabs((long long)divisor);
+      
+      long long ans = 0;
+      
+      while(a>=b){
         int shift=0;
-
-        while (a>=(b<<(shift+1))) {
+        while(a>=(b<<(shift+1))){
         shift++;
         }
-        ans+=(1LL<<shift);
-        a-=(b<<shift); // remaining
-        }
+        ans=ans+(1LL<<shift);
+        a=a-(b<<shift);
+      }
         if(negative)
         ans=-ans;
 
