c1c367493a6d4e263df1d7df5d19c0c9e4355ab3
Manoj Yadav <160408265+ManojYadav2005@users.noreply.github.com>
2026-07-21 16:10:43 +0530

    Time: 1 ms (85.49%), Space: 11.8 MB (55.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,67 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-    int maxi=0;
-    int n=s.size();
-  //  if(n==1) return 1;
-    for(int i=0;i<n;i++){
-    vector<int>vis(256,0);   
-    for(int j=i;j<n;j++){
-    if(vis[s[j]]){
-    break;
-    }
-    vis[s[j]]=1;
-    maxi=max(maxi,j-i+1);
-    }
-    } 
-    return maxi;    
+     int n=s.size();
+     int l=0;
+     int r=0;
+    int mxlen=0;
+     vector<int> hash(256, -1);
+     while(r<n){
+     if(hash[s[r]]!=-1){
+     if(hash[s[r]]>=l){
+     l=hash[s[r]]+1; }
+    
+     }
+      mxlen=max(mxlen,r-l+1);
+      hash[s[r]]=r;
+      r++;
+     }   
+     return mxlen;
     }
 };
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+
+//     bool check(int l,int r,string s){
+//     string temp;
+
+//     for(int i=l;i<=r;i++){
+//     temp=temp+s[i];
+//     }
+//     sort(temp.begin(),temp.end());
+//     int m=temp.size();
+
+//     for(int i=1;i<m;i++){
+//     if(temp[i]==temp[i-1]) return true;
+//     }
+
+//     return false;
+//     }
+
+//     int lengthOfLongestSubstring(string s) {
+//     int n=s.size();
+ 
+//     int mxlen=1;
+//     if(s.size() == 0) return 0;
+//     for(int i=0;i<n;i++){
+//     for(int j=i+1;j<n;j++){
+//     if(check(i,j,s)==false){
+//     mxlen=max(mxlen,j-i+1);
+//     }
+//     else{
+//     break;  }
+//     }
+//     }
+//     return mxlen;    
+//     }
+// };
