392d15d35ee82097ccc323fd9513c1c9233f3626
Manoj Yadav <160408265+ManojYadav2005@users.noreply.github.com>
2026-07-24 10:36:29 +0530

    Time: 0 ms (100%), Space: 26.1 MB (57.7%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -5,25 +5,30 @@ public:
     int n=height.size();
     int lftmx=0,rgtmx=0,total=0;
     int l=0,r=n-1;
+    
     while(l<r){
-    if(height[l]<=height[r]){  
+    //1. left process kro   
+    if(height[l]<=height[r]){ 
+    // Current chhota hai    
     if(lftmx>height[l]){
     total+=lftmx-height[l];  }
     else{
     lftmx=height[l];
-     }
-     l=l+1;
     }
+    l=l+1;
+    }
+    //2. right process kro
     else{
-      if(rgtmx>height[r]){
+    //Current chhota hai
+    if(rgtmx>height[r]){
     total+=rgtmx-height[r];  }
     else{
     rgtmx=height[r];
-     } 
-     r=r-1;  
+    } 
+    r=r-1;  
     }
     }
-    
+
     return total;
     }
 };
