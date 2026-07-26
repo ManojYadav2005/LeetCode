f1859cb1328f2de43e3dfc18d196ff925d1ef3cf
Manoj Yadav <160408265+ManojYadav2005@users.noreply.github.com>
2026-07-26 11:40:53 +0530

    Time: 87 ms (70.46%), Space: 239.9 MB (32.41%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -0,0 +1,95 @@
+// class Solution {
+// public:
+    
+//     bool solve(int currston_idx, int prevjp,vector<int>& stones,unordered_map<int,int>& mpp){
+    
+//     if(currston_idx == stones.size()-1)return true;
+    
+//     if (dp[currston_idx][prevjp] != -1) return dp[currston_idx][prevjp];
+//     bool result=false;
+
+//     for(int nxtjmp=prevjp-1;nxtjmp<=prevjp+1;nxtjmp++){
+//     int nxtston=stones[currston_idx]+nxtjmp;
+    
+//     if (nxtjmp<= 0) continue;
+
+//     if(mpp.find(nxtston)!=mpp.end()){
+//     result=result||solve(mpp[nxtston],nxtjmp,stones,mpp);
+    
+//     }
+//     }
+//     return result;
+//     }
+
+
+     
+//     bool canCross(vector<int>& stones) {
+   
+//     if (stones[1] != 1) return false;
+//     unordered_map<int,int>mpp;
+//     for (int i = 0; i < stones.size(); i++) {
+//     mpp[stones[i]] = i;
+//     } 
+//     vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+//     return solve(1,1,stones,mpp);
+   
+//     }
+// };
+
+
+
+
+
+class Solution {
+public:
+    vector<vector<int>> dp;
+
+    bool solve(int currston_idx,
+               int prevjp,
+               vector<int>& stones,
+               unordered_map<int,int>& mpp)
+    {
+        if (currston_idx == stones.size() - 1)
+            return true;
+
+        if (dp[currston_idx][prevjp] != -1)
+            return dp[currston_idx][prevjp];
+
+        bool result = false;
+
+        for (int nxtjmp = prevjp - 1; nxtjmp <= prevjp + 1; nxtjmp++) {
+
+            if (nxtjmp <= 0)
+                continue;
+
+            int nxtston = stones[currston_idx] + nxtjmp;
+
+            if (mpp.find(nxtston) != mpp.end()) {
+
+                result = result ||
+                         solve(mpp[nxtston], nxtjmp, stones, mpp);
+
+                if (result)
+                    break;
+            }
+        }
+
+        return dp[currston_idx][prevjp] = result;
+    }
+
+    bool canCross(vector<int>& stones) {
+
+        if (stones[1] != 1)
+            return false;
+
+        unordered_map<int,int> mpp;
+
+        for (int i = 0; i < stones.size(); i++)
+            mpp[stones[i]] = i;
+
+        int n = stones.size();
+        dp.assign(n, vector<int>(n + 1, -1));
+
+        return solve(1, 1, stones, mpp);
+    }
+};
