1a6a1b61acd6fd857f8e1ffc6e44cb6137254b06
Manoj Yadav <160408265+ManojYadav2005@users.noreply.github.com>
2026-07-26 10:46:43 +0530

    Time: 0 ms (100%), Space: 8.5 MB (53.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -21,21 +21,46 @@
 // };
 
 
+// class Solution {
+// public:
+
+//  int climbStairs(int n) {
+   
+//    if(n==1 || n==2 || n==3) return n;
+
+//    vector<int>dp(n+1);
+//    dp[0]=0;
+//    dp[1]=1;
+//    dp[2]=2;
+
+//    for(int i=3;i<=n;i++){
+//    dp[i]=dp[i-1]+dp[i-2];
+//    }
+//     return dp[n];
+//   }
+// };
+
+
+
+
+
 class Solution {
 public:
 
  int climbStairs(int n) {
    
-   if(n==1 || n==2 || n==3) return n;
+   if(n<=2) return n;
    
    vector<int>dp(n+1);
-   dp[0]=0;
-   dp[1]=1;
-   dp[2]=2;
+   
+   int prev2=1;
+   int prev1=2;
 
    for(int i=3;i<=n;i++){
-   dp[i]=dp[i-1]+dp[i-2];
+   int curr=prev1+prev2;
+   prev2=prev1;
+   prev1=curr;
    }
-    return dp[n];
+    return prev1;
   }
 };
