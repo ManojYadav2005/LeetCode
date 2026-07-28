e58d07c1b462418e089b4052ef03d103f29f16a8
Manoj Yadav <160408265+ManojYadav2005@users.noreply.github.com>
2026-07-28 11:47:17 +0530

    Time: 0 ms (100%), Space: 9.3 MB (54.67%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,19 +1,55 @@
+// class Solution {
+// public:
+//     int solve(int i,int j,int m,int n,vector<vector<int>>&dp){
+//     if(i==m-1 && j==n-1) return 1;
+    
+//     if(i<0 || i>=m || j<0 || j>=n) return 0;
+//     if(dp[i][j]!=-1) return dp[i][j]; // value de dega vo dobara same call ho rhi hai;
+    
+//     int right=solve(i,j+1,m,n,dp); // rght
+//     int down=solve(i+1,j,m,n,dp); // down
+
+//     dp[i][j]=right+down; // memorization
+//     return dp[i][j];
+//     }
+
+//     int uniquePaths(int m, int n) {
+//     vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+
+//     return solve(0,0,m,n,dp);  
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
-    int solve(int i,int j,int m,int n,vector<vector<int>>&dp){
-    if(i==m-1 && j==n-1) return 1;
-    
-    if(i<0 || i>=m || j<0 || j>=n) return 0;
-    if(dp[i][j]!=-1) return dp[i][j]; // value de dega vo dobara same call ho rhi hai;
-    int right=solve(i,j+1,m,n,dp);
-    int down=solve(i+1,j,m,n,dp);
-    dp[i][j]=right+down;
-    return dp[i][j];
-    }
 
     int uniquePaths(int m, int n) {
-    vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+    vector<vector<int>>dp(m,vector<int>(n,0));
+    
+    dp[0][0]=1;
+    
+    for(int col=1;col<n;col++){
+    dp[0][col]=1;
+    }
+    for(int row=1;row<m;row++){
+    dp[row][0]=1;
+    }
 
-    return solve(0,0,m,n,dp);  
+    for(int i=1;i<m;i++){
+    for(int j=1;j<n;j++){
+    dp[i][j]=dp[i][j-1]+dp[i-1][j];
+    }
+    }
+    return dp[m-1][n-1]; 
     }
 };
