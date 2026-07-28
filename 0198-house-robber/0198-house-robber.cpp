a555adceef8f3c6d821d2a3cbae9f2faaa4637e4
Manoj Yadav <160408265+ManojYadav2005@users.noreply.github.com>
2026-07-28 09:37:54 +0530

    Time: 0 ms (100%), Space: 10.5 MB (64.7%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int solve(int idx,vector<int>&nums,vector<int>&dp){
+    if(idx>=nums.size()) return 0;
+    
+    if(dp[idx]!=-1) return dp[idx];
+
+    int steal=nums[idx]+solve(idx+2,nums,dp);
+    int skip=solve(idx+1,nums,dp);
+    dp[idx]=max(steal,skip);
+    return dp[idx];
+    }
+
+    int rob(vector<int>& nums) {
+    int n=nums.size();    
+    vector<int>dp(n+1,-1);
+    return solve(0,nums,dp);    
+    }
+};
