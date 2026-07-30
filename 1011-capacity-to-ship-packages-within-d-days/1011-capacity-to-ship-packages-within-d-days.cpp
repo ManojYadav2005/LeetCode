1673dbafcd693a4728f973f845201d7a7d96a262
Manoj Yadav <160408265+ManojYadav2005@users.noreply.github.com>
2026-07-30 18:03:14 +0530

    Time: 14 ms (35.16%), Space: 34.9 MB (89.02%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,50 +1,51 @@
 class Solution {
 public:
-     int check(int mid,int days,vector<int>& weights, int n){
-     int day=1;
-     int load=0;
+
+    int solve(int mid,vector<int>& weights, int days){
+    int cnt=1;
+    int sm=0;
+    int n=weights.size();
 
     for(int i=0;i<n;i++){
-    if(load+weights[i]>mid) {
-    day++;
-    load=weights[i]; 
-    }
+    if(sm+weights[i]<=mid){
+    sm=sm+weights[i];
+    } 
     else{
-    load=load+weights[i];
+    cnt++;   
+    sm=weights[i];
+
+    }  
     }
+    return cnt;
     }
-    return day;
-     }
 
     int shipWithinDays(vector<int>& weights, int days) {
 
-     int n=weights.size();
-     int sum=0;
-    int maxi=0;
 
+    int n=weights.size();
+    int sm=0;
+    int maxi=INT_MIN;
     for(int i=0;i<n;i++){
-    sum+=weights[i];
+    sm=sm+weights[i];
     maxi=max(maxi,weights[i]);
     }
+    int lo=maxi;
+    int hi=sm;    
+    int ans=0;
 
-     int lo=maxi;
-     int hi=sum;
-
-     int ans=0;
-     while(lo<=hi){
-
-     int mid=lo+(hi-lo)/2;
-
-     int find=check(mid,days,weights,n);
-     if(find<=days){
-       ans=mid;
-      hi=mid-1;
-     }
-     else{
-     lo=mid+1;
-     }
-     } 
-     return ans;
-
+    while(lo<=hi){
+    int mid=lo+(hi-lo)/2;
+    
+    int res=solve(mid,weights,days);
+    if(res<=days){
+    hi=mid-1;
+    ans=mid;
+    }
+    else if(res>=days){
+    lo=mid+1;
+    
+    }
+    }
+    return ans;
     }
 };
