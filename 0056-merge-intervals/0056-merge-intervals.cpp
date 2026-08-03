d2227d9b1c693ad8ef18a653b39efc736939b3af
Manoj Yadav <160408265+ManojYadav2005@users.noreply.github.com>
2026-08-03 11:24:05 +0530

    Time: 1 ms (94.7%), Space: 23.7 MB (82.59%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+     int n=intervals.size();
+     vector<vector<int>>ans;
+     int maxi=0;
+     sort(intervals.begin(), intervals.end());
+
+     for(int i=0;i<n;i++){
+     int start=intervals[i][0];
+     int end=intervals[i][1];
+
+     int j=i+1;
+     while(j<n && intervals[j][0]<=end){
+     end=max(end,intervals[j][1]);
+     j++;
+     }
+     ans.push_back({start,end});
+     i=j-1;
+     }  
+     return ans;
+    }
+};
