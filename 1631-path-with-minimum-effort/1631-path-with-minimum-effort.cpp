56fbb0bd2b7541b1883c1ad3a46b5249d58fde45
Manoj Yadav <160408265+ManojYadav2005@users.noreply.github.com>
2026-08-08 15:46:10 +0530

    Time: 53 ms (60.62%), Space: 24.4 MB (77.28%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,51 +1,46 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-     int n=heights.size();
-     int m=heights[0].size();
+        int n = heights.size();
+        int m = heights[0].size();
 
-     vector<vector<int>>dist(n,vector<int>(m,1e9));
-     priority_queue<
-    pair<int,pair<int,int>>,
-    vector<pair<int,pair<int,int>>>,
-    greater<pair<int,pair<int,int>>>
-     > pq;
-     
-     dist[0][0]=0;
-     pq.push({0,{0,0}});
-     
-     int delrow[4]={-1,0,1,0};
-     int delcol[4]={0,1,0,-1};
+        vector<vector<int>>dist(n,vector<int>(m,1e9));
 
+        priority_queue<
+        pair<int, pair<int, int>>,
+        vector<pair<int, pair<int, int>>>,
+        greater<pair<int, pair<int, int>>>
+        >pq;
 
-     while(!pq.empty()){
+        dist[0][0] = 0;
+        pq.push({0,{0,0}});
 
-     auto it=pq.top();
-     pq.pop();
+        int delrow[4]={-1,0,1,0};
+        int delcol[4]={0,1,0,-1};
 
-     int diff=it.first;
-     int row=it.second.first;
-     int col=it.second.second;
-    
-     if(row==n-1 && col==m-1) return diff;
+        while(!pq.empty()){
 
-     for(int i=0;i<4;i++){
-     int nrow=row+delrow[i];
-     int ncol=col+delcol[i];
+        auto it=pq.top();
+        pq.pop();
+        int diffpath=it.first;
+        int row=it.second.first;
+        int col=it.second.second;
 
-    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m ){
-    int newEffort=abs(heights[row][col]-heights[nrow][ncol]);
-     newEffort=max(diff,newEffort);
-     
-     if(dist[nrow][ncol]>newEffort){
-       dist[nrow][ncol]=newEffort; 
-     pq.push({newEffort,{nrow,ncol}});
-     }
-     
-     }
-     }
-     }
+        for(int i=0;i<4;i++) {
+        int nrow=row+delrow[i];
+        int ncol=col+delcol[i];
+        
+        if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
 
-    return 0;
+        int curdiff=abs(heights[row][col]-heights[nrow][ncol]);
+        curdiff=max(diffpath,curdiff);
+
+        if(curdiff<dist[nrow][ncol]){
+        dist[nrow][ncol]=curdiff;
+        pq.push({curdiff,{nrow,ncol}}); }
+        }
+        }
+        }
+        return dist[n-1][m-1];
     }
 };
