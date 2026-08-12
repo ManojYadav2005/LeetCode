a2c293b10d280530d301e248769da0a037b002af
Manoj Yadav <160408265+ManojYadav2005@users.noreply.github.com>
2026-08-12 09:07:26 +0530

    Time: 0 ms (100%), Space: 95.1 MB (80.09%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,42 +1,87 @@
+// class Solution {
+// public:
+//     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+//      if(nums1.size()>nums2.size()){
+//      return findMedianSortedArrays(nums2,nums1);
+//      }  
+//      int n=nums1.size();
+//      int m=nums2.size();
+
+//      int l=0;
+//      int r=n;
+
+//      while(l<=r){
+     
+//      int Px=l+(r-l)/2;   // mid from nums1;
+//      int Py=(m+n+1)/2-Px; //  from nums2;
+      
+//       // left wala part
+//      int x1=(Px == 0) ? INT_MIN : nums1[Px-1];
+//      int x2=(Py == 0) ? INT_MIN : nums2[Py-1];
+     
+//      // left wala part
+//      int x3=(Px == n) ? INT_MAX : nums1[Px];
+//      int x4=(Py == m) ? INT_MAX :nums2[Py];
+
+//      if(x1<=x4 && x2<=x3){
+//      if((m+n)%2==1){
+//      return max(x1,x2);
+//      }
+//      else{
+//         return (max(x1,x2)+min(x3,x4))/2.0;
+//      }
+//      }
+
+//      if(x1>x4) r=Px-1;
+//      else l=Px+1;
+
+//      }
+//      return -1;
+//     }
+// };
+
+
+
+
+
+
+
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 
-     if(nums1.size()>nums2.size()){
-     return findMedianSortedArrays(nums2,nums1);
-     }  
-     int n=nums1.size();
-     int m=nums2.size();
+    if(nums1.size()>nums2.size()){
+    return findMedianSortedArrays(nums2,nums1);
+    }
+    int n=nums1.size();
+    int m=nums2.size();
+    int lo=0;
+    int hi=n;
 
-     int l=0;
-     int r=n;
+    while(lo<=hi){
+    int Px=lo+(hi-lo)/2;
+    int Py=(m+n+1)/2-Px;
 
-     while(l<=r){
-     
-     int Px=l+(r-l)/2;   // mid from nums1;
-     int Py=(m+n+1)/2-Px;
-      
-      // left wala part
-     int x1=(Px == 0) ? INT_MIN : nums1[Px-1];
-     int x2=(Py == 0) ? INT_MIN : nums2[Py-1];
-     
-     // left wala part
-     int x3=(Px == n) ? INT_MAX : nums1[Px];
-     int x4=(Py == m) ? INT_MAX :nums2[Py];
+    int x1=(Px==0) ?INT_MIN :nums1[Px-1];
+    int x2=(Py==0) ?INT_MIN :nums2[Py-1];
+
+    int x3=(Px==n) ?INT_MAX :nums1[Px];
+
+    int x4=(Py==m) ?INT_MAX :nums2[Py];
 
      if(x1<=x4 && x2<=x3){
      if((m+n)%2==1){
      return max(x1,x2);
      }
-     else{
-        return (max(x1,x2)+min(x3,x4))/2.0;
-     }
+      else{
+      return (max(x1,x2)+min(x3,x4))/2.0;
+      }
      }
 
-     if(x1>x4) r=Px-1;
-     else l=Px+1;
-
-     }
-     return -1;
+     if(x1>x4) hi=Px-1;
+     else lo=Px+1;
+    }
+    return -1;
     }
 };
