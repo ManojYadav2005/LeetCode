91d0a91f5426745f5bd8a24eaca2ff3024d4c990
Manoj Yadav <160408265+ManojYadav2005@users.noreply.github.com>
2026-08-15 12:26:51 +0530

    Time: 0 ms (100%), Space: 26 MB (77.82%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,41 +1,30 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-
-        int n = height.size();
-        stack<int> st;   // stores indices
-        int total = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            // Current building is greater than stack top
-            // So a valley is getting closed
-            while (!st.empty() && height[i] > height[st.top()]) {
-
-                int bottom = st.top();
-                st.pop();
-
-                // No left boundary
-                if (st.empty())
-                    break;
-
-                int left = st.top();
-                int right = i;
-
-                // Width between left and right boundary
-                int width = right - left - 1;
-
-                // Height of water
-                int waterHeight =
-                    min(height[left], height[right])
-                    - height[bottom];
-
-                total += width * waterHeight;
-            }
-
-            st.push(i);
-        }
-
-        return total;
+     int n=height.size();
+     int l=0;
+     int r=n-1;
+     int sm=0;
+     int lftmx=0;
+     int rghtmx=0;
+     while(l<r){
+     // case 1
+     if(height[l]<=height[r]){
+     if(lftmx>height[l]){
+     sm=sm+(lftmx-height[l]);
+     }
+     else lftmx=height[l];
+     l++;
+     }
+     // case 2
+     else{
+      if(rghtmx>height[r]){
+      sm=sm+(rghtmx-height[r]);
+      }
+      else rghtmx=height[r];
+      r--;
+     }
+     } 
+     return sm;  
     }
 };
