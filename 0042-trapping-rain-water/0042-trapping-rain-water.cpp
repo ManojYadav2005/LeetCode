7d885487389841b0e8e2ca9667460accc1d0a9aa
Manoj Yadav <160408265+ManojYadav2005@users.noreply.github.com>
2026-08-15 11:59:24 +0530

    Time: 0 ms (100%), Space: 27 MB (37.49%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-  
-    int n=height.size();
-    int lftmx=0,rgtmx=0,total=0;
-    int l=0,r=n-1;
-    
-    while(l<r){
-    //1. left process kro   
-    if(height[l]<=height[r]){ 
-    // Current chhota hai    
-    if(lftmx>height[l]){
-    total+=lftmx-height[l];  }
-    else{
-    lftmx=height[l];
-    }
-    l=l+1;
-    }
-    //2. right process kro
-    else{
-    //Current chhota hai
-    if(rgtmx>height[r]){
-    total+=rgtmx-height[r];  }
-    else{
-    rgtmx=height[r];
-    } 
-    r=r-1;  
-    }
-    }
 
-    return total;
+        int n = height.size();
+        stack<int> st;   // stores indices
+        int total = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            // Current building is greater than stack top
+            // So a valley is getting closed
+            while (!st.empty() && height[i] > height[st.top()]) {
+
+                int bottom = st.top();
+                st.pop();
+
+                // No left boundary
+                if (st.empty())
+                    break;
+
+                int left = st.top();
+                int right = i;
+
+                // Width between left and right boundary
+                int width = right - left - 1;
+
+                // Height of water
+                int waterHeight =
+                    min(height[left], height[right])
+                    - height[bottom];
+
+                total += width * waterHeight;
+            }
+
+            st.push(i);
+        }
+
+        return total;
     }
 };
