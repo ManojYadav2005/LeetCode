9b311c886da39ed57cf88c69f0b3200e04e364e3
Manoj Yadav <160408265+ManojYadav2005@users.noreply.github.com>
2026-08-24 10:51:10 +0530

    Time: 0 ms (100%), Space: 8.1 MB (47.78%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+    int n=s.size();
+    int mini=0;
+    int maxi=0;
+    
+    for(int i=0;i<n;i++){
+    if(s[i]=='('){
+    mini++;
+    maxi++;
+    }
+    else if(s[i]==')'){
+    mini=mini-1;
+    maxi=maxi-1;
+    }
+    else{
+    mini=mini-1;
+    maxi=maxi+1;
+    }
+    if(mini<0) mini=0;
+    if(maxi<0) return false;
+    } 
+    
+    if(mini==0) return true;
+    else return false;   
+    }
+};
