e22e42924a53de9423e63c067428d2f28291cc3d
Manoj Yadav <160408265+ManojYadav2005@users.noreply.github.com>
2026-08-31 00:49:02 +0530

    Time: 330 ms (94.16%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT s1.id,
+COALESCE(s2.student,s1.student) as student
+FROM Seat s1
+LEFT JOIN Seat s2
+ON s2.id=CASE
+WHEN s1.id%2=1 THEN s1.id+1
+ELSE s1.id-1
+END
+ORDER BY s1.id
